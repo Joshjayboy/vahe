8ff3ee47b0ec7c9ec58a1710fd293a72d70bc06e
Joshjayboy <jakintemi@gmail.com>
2023-07-04 09:23:50 +0100

    n

--- src/Components/ForgetPassword.js
+++ src/Components/ForgetPassword.js
@@ -12,6 +12,7 @@ import {
   Box,
   Typography,
 } from "@mui/material";
+import { ReactCodeInput } from "react-code-input";
 // import { Typography } from "@mui/material";
 
 const SendOTPForm = ({
@@ -122,20 +123,105 @@ const SendOTPForm = ({
 );
 
 const VerifyOTPForm = ({ onSubmit, otpCode, setOtpCode }) => (
-  <form onSubmit={onSubmit}>
-    <TextField
-      label="OTP"
-      type="text"
-      variant="outlined"
-      fullWidth
-      value={otpCode}
-      onChange={(e) => setOtpCode(e.target.value)}
-      required
-    />
-    <Button type="submit" variant="contained">
-      Verify OTP
-    </Button>
-  </form>
+  <>
+    <Box container sx={{ minHeight: "600px", marginTop: "100px" }}>
+      <Box
+        sx={{ display: "flex", marginBottom: "32px", justifyContent: "center" }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <Box
+            component="span"
+            sx={{
+              color: "black",
+              fontWeight: "700",
+              paddingBottom: "24px",
+              fontSize: "34px",
+            }}
+          >
+            Verify your phone number
+          </Box>
+          <Box>The message with verification code will be sent to number</Box>
+
+          <Box
+            sx={{
+              fontSize: "20px",
+              marginTop: "32px",
+              textAlign: "center",
+              fontWeight: "bold",
+            }}
+          >
+            jakintemi@gmail.com
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              marginTop: "32px",
+              justifyContent: "center",
+            }}
+          >
+            <form onSubmit={onSubmit}>
+              <TextField
+                label="OTP"
+                type="text"
+                variant="outlined"
+                fullWidth
+                value={otpCode}
+                onChange={(e) => setOtpCode(e.target.value)}
+                required
+              />
+              {/* <Button type="submit" variant="contained">
+                Verify OTP
+              </Button> */}
+            </form>
+          </Box>
+          <Box
+            sx={{
+              cursor: "pointer",
+              color: "rgb(3, 188, 102)",
+              marginTop: "32px",
+            }}
+          >
+            Resend verification code
+          </Box>
+          <Box sx={{ width: "100%", marginTop: "32px" }}>
+            <Button
+              type="submit"
+              variant="contained"
+              onSubmit={onSubmit}
+              sx={{
+                padding: "10px 18px",
+                width: "100%",
+                fontWeight: "bold",
+                lineHeight: "22px",
+                textTransform: "inherit",
+                boxShadow: "none",
+                borderRadius: "10px",
+                background:
+                  "transparent linear-gradient(180deg, #c5022e 0%, #ea1f4d 100%) 0% 0% no-repeat",
+              }}
+            >
+              <span
+                sx={{
+                  width: "100%",
+                  display: "inherit",
+                  alignItems: "inherit",
+                  justifyContent: "inherit",
+                }}
+              >
+                Verify OTP
+              </span>
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  </>
 );
 
 const ChangePasswordForm = ({
